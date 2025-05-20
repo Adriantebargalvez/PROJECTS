7e14f3ba327570ee5c0ccc3511188acdf9807aad
Adriantebargalvez <124157818+Adriantebargalvez@users.noreply.github.com>
2025-05-20 21:45:31 +0200

    cambios

--- AngularClaseAPP/Tienda/src/app/common/user.ts
+++
@@ -1,9 +0,0 @@
-export interface User{
-    uid?: string,
-    email: string,
-    displayName: string,
-    photoURL?: string,
-    emailVerified:boolean,
-    comentarios?: {displayName: string,comentario: string, fecha: string }[];
-    clasificaciones?: { articuloId: string; rating: number }[];
-}
